3aaef4876ee087d52410e83aa8207b6e92de8ace
Aman <aman13xrestha@gmail.com>
2022-01-25 21:39:32 +0545

    changes to link

--- src/components/milstones/Milstones.js
+++ src/components/milstones/Milstones.js
@@ -1,33 +1,36 @@
-import React from 'react';
-import './Milestones.css'
+import React from "react";
+import "./Milestones.css";
+import { Link } from "react-router-dom";
 const Milstones = () => {
-    return (
+  return (
     <div className="featured">
-
+      <Link to="/donate">
         <div className="featuredItem">
-            <span className="featuredItemName">Donation Projects</span>
-            <div classname="featuredContainer">
-                <span className="featuredItemCount">13</span>
-            </div>
+          <span className="featuredItemName">Donation Projects</span>
+          <div classname="featuredContainer">
+            <span className="featuredItemCount">13</span>
+          </div>
         </div>
+      </Link>
 
+      <Link to="/beneficiary">
         <div className="featuredItem">
-            <span className="featuredItemName">Benefeceries</span>
-            <div classname="featuredContainer">
-                <span className="featuredItemCount">8</span>
-            </div>
+          <span className="featuredItemName">Benefeceries</span>
+          <div classname="featuredContainer">
+            <span className="featuredItemCount">8</span>
+          </div>
         </div>
-
+      </Link>
+      <Link to="/vendor">
         <div className="featuredItem">
-            <span className="featuredItemName">Vendors</span>
-            <div classname="featuredContainer">
-                <span className="featuredItemCount">4</span>
-            </div>
+          <span className="featuredItemName">Vendors</span>
+          <div classname="featuredContainer">
+            <span className="featuredItemCount">4</span>
+          </div>
         </div>
-
-
+      </Link>
     </div>
-    )
-}
+  );
+};
 
 export default Milstones;
