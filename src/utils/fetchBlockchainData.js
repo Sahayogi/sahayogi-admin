a0e3353e7a100fc43590c00abd78810238679381
aman007shrestha <aman13xrestha@gmail.com>
2022-03-03 18:49:40 +0545

    attempt to add projId

--- src/utils/fetchBlockchainData.js
+++ src/utils/fetchBlockchainData.js
@@ -1,7 +1,6 @@
-import { getProjectCount } from "../pages/Web3Client";
+import { getProjectCount } from '../pages/Web3Client';
 
-export const countOfProject =()=>{
-    const pcount = getProjectCount();
-    return pcount;
-
-}
+export const countOfProject = async () => {
+  const pcount = await getProjectCount();
+  return pcount;
+};
