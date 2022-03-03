0c782b1f77e7098b09f1b5cd373d4cb5cc334f6e
aman007shrestha <aman13xrestha@gmail.com>
2022-03-03 14:38:19 +0545

    Projects loader

--- src/pages/project/Projects.js
+++ src/pages/project/Projects.js
@@ -41,7 +41,34 @@ const AddDiv = styled.div`
   font-size: 30px;
   cursor: pointer;
 `;
+const MainLoader = styled.div`
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  /* border: 2px solid white; */
+`;
 
+const Loader = styled.div`
+  flex: 1;
+  margin: auto;
+  margin-top: 200px;
+  margin-bottom: 200px;
+  height: calc(100vh);
+  border: 16px solid #f3f3f3;
+  border-top: 16px solid black;
+  border-radius: 50%;
+  width: 130px;
+  height: 130px;
+  animation: spin 0.5s linear infinite;
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
 const Projects = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -82,43 +109,54 @@ const Projects = () => {
           <AddDiv> + Add Projects</AddDiv>
         </Link>
       )}
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label='simple table'>
-          <TableHead>
-            <TableRow>
-              <TableCell>Id</TableCell>
-              <TableCell align='center'>Donation Projects</TableCell>
-              <TableCell align='center'>Number of Beneficiaries</TableCell>
-              <TableCell align='center'>Tokens</TableCell>
-              <TableCell align='center'>Status</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {posts.map((row, index) => (
-              <TableRow
-                key={row._id}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component='th' scope='row'>
-                  {index + 1}
-                </TableCell>
-                <TableCell align='center'>
-                  <Link to={`/projects/${row._id}`}>
-                    <ProjectName>{row.projectName}</ProjectName>
-                  </Link>
-                </TableCell>
-                <TableCell align='center'>{row.beneficiaries.length}</TableCell>
-                <TableCell align='center'>{row.collectedToken}</TableCell>
-                <TableCell align='center'>
-                  <button className='statusButton'>
-                    {row.status === true ? 'Active' : 'Unverified'}
-                  </button>
-                </TableCell>
+      {loading && (
+        <div>
+          <MainLoader>
+            <Loader></Loader>
+          </MainLoader>
+        </div>
+      )}
+      {!loading && (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+            <TableHead>
+              <TableRow>
+                <TableCell>Id</TableCell>
+                <TableCell align='center'>Donation Projects</TableCell>
+                <TableCell align='center'>Number of Beneficiaries</TableCell>
+                <TableCell align='center'>Tokens</TableCell>
+                <TableCell align='center'>Status</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {posts.map((row, index) => (
+                <TableRow
+                  key={row._id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component='th' scope='row'>
+                    {index + 1}
+                  </TableCell>
+                  <TableCell align='center'>
+                    <Link to={`/projects/${row._id}`}>
+                      <ProjectName>{row.projectName}</ProjectName>
+                    </Link>
+                  </TableCell>
+                  <TableCell align='center'>
+                    {row.beneficiaries.length}
+                  </TableCell>
+                  <TableCell align='center'>{row.collectedToken}</TableCell>
+                  <TableCell align='center'>
+                    <button className='statusButton'>
+                      {row.status === true ? 'Active' : 'Unverified'}
+                    </button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
     </Container>
   );
 };
